57b1a6848eaaac47038a17f53dd47ce6ee89ee42
Emilly Paiva <emillypaiva3260@gmail.com>
2025-10-21 14:34:26 -0300

    🧩 chore: IBook Provider update

--- src/modules/books/domain/provideres/IBookProvider.js
+++ src/modules/books/domain/provideres/IBookProvider.js
@@ -2,4 +2,8 @@ export class IBookProvider {
   async searchByName(name) {
     throw new Error("Method 'searchByName' must be implemented.");
   }
+
+  async findById(googleBookId) {
+    throw new Error("Method 'findById' must be implemented.");
+  }
 }
