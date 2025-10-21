7d551c88fa126f8c14ec5b3d7e43c70a24933963
Emilly Paiva <emillypaiva3260@gmail.com>
2025-10-21 14:31:39 -0300

    🛣️ chore: update book routes with new endpoin

--- src/modules/books/presentation/routes/bookRoutes.js
+++ src/modules/books/presentation/routes/bookRoutes.js
@@ -1,11 +1,17 @@
 
 import { Router } from 'express';
 import { searchBooksController } from '../../application/useCases/searchBooks/index.js';
+import { getBookByIdController } from '../../application/useCases/getBookById/index.js';
 
 const bookRoutes = Router();
 
 bookRoutes.get('/search', (request, response) => {
   return searchBooksController.handle(request, response);
+});
+
+bookRoutes.get('/:id', (request, response) => {
+  
+  return getBookByIdController.handle(request, response);
 });
 
 export { bookRoutes };
